024fbf025f8ece5b896a39493d4cc98641c27b2d
Mustafa <byrmstf@yandex.com>
2021-08-27 15:10:17 +0300

    php adding

---
+++ img.php
@@ -0,0 +1,17 @@
+<?php
+
+// print_r($_SERVER['HTTP_REFERER']); die();
+
+if($_SERVER['HTTP_REFERER'] !== 'http://hide-url/'){
+die();
+}
+
+$images = array(
+    '05edd57091ad570303df856c652a7a174554a148' => 'sample.pdf',
+);
+
+$filehash = $_GET['h'];
+$filename = "documents/".$images[$filehash];
+header('Content-Type: application/pdf');
+readfile($filename);
+?>
---
+++ index.php
@@ -0,0 +1,15 @@
+<?php
+$hash = "05edd57091ad570303df856c652a7a174554a148";
+?>
+<!DOCTYPE html>
+<html lang="tr" dir="ltr">
+    <head>
+        <meta charset="UTF-8">
+        <title>TEST</title>
+    </head>
+    <body>
+        <object data="/img.php?h=<?= $hash ?>" type="application/pdf" width="100%" height="800px">
+            <embed width="100%" height="1000px" src="/img.php?h=<?= $hash ?>"/>
+        </object>
+    </body>
+</html>
