8272a1cf5c77c8f5bf24946c2224d0e310c10d08
Mustafa <byrmstf@yandex.com>
2022-03-25 23:52:05 +0300

    HTTP_REFERER bug fixed

--- img.php
+++ img.php
@@ -1,12 +1,11 @@
 <?php
-if($_SERVER['HTTP_REFERER'] !== 'http://hide-url/')
+if($_SERVER['HTTP_REFERER'] !== 'http://hidden-url/index.php')
     die();
 
 
 $images = array(
     '05edd57091ad570303df856c652a7a174554a148' => 'sample.pdf',
 );
-
 $filehash = $_GET['h'];
 $filename = "documents/".$images[$filehash];
 header('Content-Type: application/pdf');
--- index.php
+++ index.php
@@ -8,6 +8,7 @@ $hash = "05edd57091ad570303df856c652a7a174554a148";
         <title>TEST</title>
     </head>
     <body>
+        <h1>PDF hidden URL</h1>
         <object data="/img.php?h=<?= $hash ?>" type="application/pdf" width="100%" height="800px">
             <embed width="100%" height="1000px" src="/img.php?h=<?= $hash ?>"/>
         </object>
